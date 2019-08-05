eaba46fcbbf4993389117eb8a1a3cd0dad3fd7a7
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2019-08-05 13:21:10 -0300

    youtube list channel

---
+++ src/main/java/br/com/obviobrasil/ChannelList.java
@@ -0,0 +1,100 @@
+package br.com.obviobrasil;
+/*
+ * Copyright (c) 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import java.io.File;
+import java.util.List;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.java6.auth.oauth2.FileCredentialStore;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.Channel;
+import com.google.api.services.youtube.model.ChannelListResponse;
+import com.google.api.services.youtube.model.ChannelSnippet;
+import com.google.common.collect.Lists;
+
+/**
+ * Create a video bulletin that is posted to the user's channel feed.
+ *
+ * @author Jeremy Walker
+ */
+public class ChannelList {
+	/** Global instance of the HTTP transport. */
+	private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+
+	/** Global instance of the JSON factory. */
+	private static final JsonFactory JSON_FACTORY = new JacksonFactory();
+
+	/**
+	 * Authorizes the installed application to access user's protected data.
+	 *
+	 * @param scopes list of scopes needed to run upload.
+	 */
+	private static String getToken(List<String> scopes) throws Exception {
+		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
+				ChannelList.class.getResourceAsStream("/client_secrets.json"));
+		final String home = System.getProperty("user.home");
+		final File store = new File(home, ".credentials/channel-list.json");
+		FileCredentialStore credentialStore = new FileCredentialStore(store, JSON_FACTORY);
+		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
+				clientSecrets, scopes).setCredentialStore(credentialStore).build();
+		LocalServerReceiver localReceiver = new LocalServerReceiver.Builder().setPort(8080).build();
+		return new AuthorizationCodeInstalledApp(flow, localReceiver).authorize("user").getAccessToken();
+	}
+
+	/**
+	 * Authorize the user, call the youtube.channels.list method to retrieve
+	 * information about the user's YouTube channel, and post a bulletin with a
+	 * video ID to that channel.
+	 */
+	public static void main(String[] args) {
+		List<String> scopes = Lists.newArrayList("https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/youtube");
+		try {
+			final String token = getToken(scopes);
+			System.err.println(token);
+			final Credential credential = new GoogleCredential().setAccessToken(token);
+			YouTube youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName("Hugme")
+					.build();
+			YouTube.Channels.List channelRequest = youtube.channels().list("id,snippet,contentDetails");
+			channelRequest.setMine("true");
+			ChannelListResponse channelResult = channelRequest.execute();
+			List<Channel> channelsList = channelResult.getItems();
+			if (channelsList != null) {
+				for (Channel channel : channelsList) {
+					final ChannelSnippet snippet = channel.getSnippet();
+					System.out.println(channel.getId() + ": " + snippet.getTitle());
+				}
+			} else {
+				System.out.println("No channels are assigned to this user.");
+			}
+		} catch (GoogleJsonResponseException e) {
+			e.printStackTrace();
+			System.err.println(
+					"There was a service error: " + e.getDetails().getCode() + " : " + e.getDetails().getMessage());
+
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+}
